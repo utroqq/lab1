3da5ae1310b40666fadd2b471b7003c642e1ffb8
unknown <ilacaregorodcev151@gmail.com>
2026-04-14 20:12:53 +0600

    laba2new

--- app.py
+++ app.py
@@ -1,31 +1,48 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect, url_for
 import random
 
 app = Flask(__name__)
-app.secret_key = 'secret123'  # нужно для хранения ответа
+app.secret_key = "secret123"
 
-def generate_captcha():
+
+def new_captcha():
     a = random.randint(1, 10)
     b = random.randint(1, 10)
-    session['answer'] = a + b
-    return f"{a} + {b}"
+    session["answer"] = a + b
+    session["captcha_text"] = f"{a} + {b}"
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def index():
     message = ""
 
-    if request.method == 'POST':
-        user_answer = request.form.get('captcha')
+    if request.method == "POST":
+        user_answer = request.form.get("captcha")
 
-        if user_answer and int(user_answer) == session.get('answer'):
-            message = "Ты не робот ✅"
-        else:
-            message = "Неправильно ❌"
+        if user_answer and session.get("answer") is not None:
+            if int(user_answer) == session["answer"]:
+                return redirect(url_for('success'))
+            else:
+                message = "Неправильно"
 
-    captcha = generate_captcha()
-    return render_template('index.html', captcha=captcha, message=message)
+        new_captcha()
 
-if __name__ == '__main__':
+    if "answer" not in session:
+        new_captcha()
+
+    return render_template(
+        "index.html",
+        captcha=session["captcha_text"],
+        message=message
+    )
+
+
+@app.route("/success")
+def success():
+    return redirect("https://google.com")
+
+
+if __name__ == "__main__":
     app.run(debug=True)
 
 #http://127.0.0.1:5000
